f52843333a37bfff806da2f43cdac65b8034432c
hiro200 <hooahdev@HooAhui-iMac.local>
2019-02-25 13:46:02 +0900

    Expanding Footer

--- ExpandingCell/ExpandingController.swift
+++ ExpandingCell/ExpandingController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+//import PullToRefreshKit
 
 
 class ExpandingController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
@@ -18,7 +19,7 @@ class ExpandingController: UIViewController, UICollectionViewDelegate, UICollect
     @IBOutlet weak var ExpandCollectionView: UICollectionView!
     
     let inspirations = Inspiration.allInspirations()
-    
+    private let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +35,39 @@ class ExpandingController: UIViewController, UICollectionViewDelegate, UICollect
         }
     */
         
+        
+        if #available(iOS 10.0, *) {
+            ExpandCollectionView.refreshControl = refreshControl
+        } else {
+            ExpandCollectionView.addSubview(refreshControl)
+        }
+        
+        ExpandCollectionView.register(UINib(nibName: "ExpandingFooterCell", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "ExpandingFooterCell")
+        
         ExpandCollectionView.register(UINib(nibName: "ExpandingCell", bundle: nil), forCellWithReuseIdentifier: "ExpandingCell")
         
+        
+        
+       refreshControl.addTarget(self, action: #selector(refreshWeatherData(_:)), for: .valueChanged)
+        
+        
+        refreshControl.tintColor = UIColor(red:0.25, green:0.72, blue:0.85, alpha:1.0)
+        
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching Weather Data ...", attributes: nil)
     }
     
+    
+    @objc private func refreshWeatherData(_ sender: Any) {
+        // Fetch Weather Data
+        fetchWeatherData()
+    }
+    
+    
+    private func fetchWeatherData() {
+        
+                self.refreshControl.endRefreshing()
+
+    }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -53,6 +83,18 @@ class ExpandingController: UIViewController, UICollectionViewDelegate, UICollect
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionFooter {
+            let aFooterView = ExpandCollectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ExpandingFooterCell", for: indexPath) as! ExpandingFooterCell
+            
+            
+            
+            return aFooterView
+        } else {
+            let headerView = ExpandCollectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ExpandingFooterCell", for: indexPath)
+            return headerView
+        }
+    }
     
     
     
---
+++ ExpandingCell/ExpandingFooterCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingFooterCell.swift
+//  AwesomeChat
+//
+//  Created by Good Luck on 24/02/2019.
+//  Copyright © 2019 GoodLuck. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExpandingFooterCell: UICollectionViewCell {
+    @IBOutlet weak var cellTextLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
